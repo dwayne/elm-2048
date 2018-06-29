c8396651b08700abc5e04ba13be2ec3b6ae0c660
Dwayne Crooks <me@dwaynecrooks.com>
2018-06-29 14:25:32 -0400

    Start with a very simple layout

--- Game.elm
+++ Game.elm
@@ -1,5 +1,29 @@
 module Game exposing (main)
 
-import Html
+import Html exposing (..)
 
-main = Html.text "Hello, Elm!"
+main : Html msg
+main =
+  view model
+
+-- MODEL
+
+type alias Model =
+  { score : Int
+  }
+
+model : Model
+model =
+  { score = 0
+  }
+
+-- VIEW
+
+view : Model -> Html msg
+view { score } =
+  div []
+    [ h1 [] [ text "Elm 2048" ]
+    , p [] [ text <| "Score: " ++ (toString score) ]
+    , p [] [ button [] [ text "New Game" ] ]
+    , div [] [ text "The grid will appear here." ]
+    ]

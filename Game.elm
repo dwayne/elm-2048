c563a7fd6bb29d7f1d704f0bc66badf0f93e260f
Dwayne Crooks <me@dwaynecrooks.com>
2018-07-10 21:06:27 -0400

    Display 2 tiles in random positions to start

--- Game.elm
+++ Game.elm
@@ -3,6 +3,7 @@ module Game exposing (main)
 import Dict
 import Html exposing (..)
 import Html.Events as Events
+import Random exposing (Generator)
 import Svg exposing (Svg)
 import Svg.Attributes
 
@@ -36,24 +37,12 @@ type alias Tile =
 
 init : (Model, Cmd Msg)
 init =
-  emptyModel ! []
+  emptyModel ! [ newGrid ]
 
 emptyModel : Model
 emptyModel =
   { score = 0
-  , grid =
-      [ { row = 0, col = 0, value = 2 }
-      , { row = 0, col = 1, value = 4 }
-      , { row = 0, col = 2, value = 8 }
-      , { row = 0, col = 3, value = 16 }
-      , { row = 1, col = 0, value = 32 }
-      , { row = 1, col = 1, value = 64 }
-      , { row = 1, col = 2, value = 128 }
-      , { row = 1, col = 3, value = 256 }
-      , { row = 2, col = 0, value = 512 }
-      , { row = 2, col = 1, value = 1024 }
-      , { row = 2, col = 2, value = 2048 }
-      ]
+  , grid = []
   }
 
 
@@ -62,12 +51,20 @@ emptyModel =
 
 type Msg
   = NewGame
+  | NewGrid Grid
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     NewGame ->
-      emptyModel ! []
+      emptyModel ! [ newGrid ]
+
+    NewGrid grid ->
+      { model | grid = grid } ! []
+
+newGrid : Cmd Msg
+newGrid =
+  Random.generate NewGrid start
 
 
 -- VIEW
@@ -179,6 +176,88 @@ viewTile { row, col, value } =
       ]
 
 
+-- GRID
+
+
+-- Generate a grid with two tiles in any of the available positions.
+start : Generator Grid
+start =
+  twoTiles []
+    |> Random.map (\(tile1, tile2) -> [tile1, tile2])
+
+-- Generate one tile in an available position.
+--
+-- N.B. It assumes that at least one position is available.
+oneTile : List Tile -> Generator Tile
+oneTile tiles =
+  let
+    get : Int -> List (Int, Int) -> (Int, Int)
+    get index list =
+      list
+        |> List.drop index
+        |> List.head
+        |> Maybe.withDefault (0, 0)
+
+    possibilities =
+      availableCells tiles
+
+    maxIndex =
+      List.length possibilities - 1
+  in
+    Random.pair (Random.int 0 maxIndex) valueGen
+      |> Random.map
+        (\(index, value) ->
+          let
+            (row, col) =
+              get index possibilities
+          in
+            { row = row, col = col, value = value }
+        )
+
+-- Generate two tiles in available positions.
+--
+-- N.B. It assumes that at least two positions are available.
+twoTiles : List Tile -> Generator (Tile, Tile)
+twoTiles tiles =
+  oneTile tiles
+    |> Random.andThen
+      (\tile ->
+        oneTile (tile :: tiles)
+          |> Random.map ((,) tile)
+      )
+
+valueGen : Generator Int
+valueGen =
+  Random.float 0 1
+    |> Random.map (\x -> if x < minValueProb then minValue else 2 * minValue)
+
+-- Determines all the available cell positions.
+--
+-- For e.g.
+--
+-- availableCells
+--   [ { row = 0, col = 1, value = 4 }, { row = 2, col = 3, value = 16 } ]
+-- =>
+-- [ (0, 0), (0, 2), (0, 3)
+-- , (1, 0), (1, 1), (1, 2), (1, 3)
+-- , (2, 0), (2, 1), (2, 2)
+-- , (3, 0), (3, 1), (3, 2), (3, 3)
+-- ]
+availableCells : List Tile -> List (Int, Int)
+availableCells tiles =
+  let
+    allPositions =
+      cartesianMap cellCount cellCount (,)
+  in
+    List.filter
+      (\(row, col) ->
+        List.all
+          (\tile -> tile.row /= row || tile.col /= col)
+          tiles
+      )
+      allPositions
+
+
 -- CONFIG
 
 
@@ -203,6 +282,13 @@ cellDistance n =
 
 cellColor : String
 cellColor = "rgba(238, 228, 218, 0.35)"
+
+minValue : Int
+minValue = 2
+
+-- The probability for randomly choosing the minimum value.
+minValueProb : Float
+minValueProb = 0.9
 
 type alias TileInfo =
   { color : String

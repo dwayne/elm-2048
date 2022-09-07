f1fa8aef466092eaf43bff97f2b1bbbad4d1ebce
Dwayne Crooks <me@dwaynecrooks.com>
2022-09-07 08:32:11 -0400

    Start building experiments to understand how all the animations will work

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b42e50fe36242b1b205a7d501b7911d698218086.tar.gz") {}
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.caddy
+  ];
+}

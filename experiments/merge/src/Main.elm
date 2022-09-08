376fce46f3e85e364f49fad1e85852a993ef30ef
Dwayne Crooks <me@dwaynecrooks.com>
2022-09-08 08:49:44 -0400

    Figure out the mechanics of moving and animating tiles

--- experiments/merge/src/Main.elm
+++ experiments/merge/src/Main.elm
@@ -20,84 +20,266 @@ main =
 
 
 type alias Model =
-  { tiles : List Tile
+  { testCase : TestCase
+  , grid : Grid
   }
 
 
+type alias TestCase =
+  { before : Grid
+  , after : Grid
+  }
+
+
+type alias Grid =
+  List Tile
+
+
 type alias Tile =
-  { position : (Int, Int)
+  { kind : Kind
+  , position : Position
   , value : Int
-  , kind : Kind
+  , action : Action
   }
 
 
 type Kind
   = New
-  | MoveTo (Int, Int)
-  | Merged
+  | Stale
+  | Composite
+
+
+type alias Position =
+  (Int, Int)
+
+
+type Action
+  = Stay
+  | MoveTo Position
+
+
+testCase1 : TestCase
+testCase1 =
+  { before =
+      [ Tile New (1, 1) 2 Stay
+      , Tile New (2, 1) 2 Stay
+      , Tile New (2, 3) 4 Stay
+      , Tile New (3, 1) 2 Stay
+      , Tile New (3, 3) 2 Stay
+      , Tile New (4, 4) 2 Stay
+      ]
+  , after =
+      [ Tile Stale (1, 1) 2 <| MoveTo (1, 4)
+      , Tile Stale (2, 1) 2 <| MoveTo (2, 3)
+      , Tile Stale (2, 3) 4 <| MoveTo (2, 4)
+      , Tile Stale (3, 1) 2 <| MoveTo (3, 4)
+      , Tile Stale (3, 3) 2 <| MoveTo (3, 4)
+      , Tile Stale (4, 4) 2 Stay
+      , Tile Composite (3, 4) 4 Stay
+      -- N.B. The action for Composite will always be Stay.
+      -- Similarly, for New. Means we can reorganize the Tile type.
+      ]
+  }
+
+
+testCase2 : TestCase
+testCase2 =
+  { before =
+      [ Tile New (1, 4) 2 Stay
+      , Tile New (2, 2) 4 Stay
+      , Tile New (2, 4) 2 Stay
+      , Tile New (3, 2) 2 Stay
+      , Tile New (3, 4) 2 Stay
+      , Tile New (4, 1) 2 Stay
+      ]
+  , after =
+      [ Tile Stale (1, 4) 2 <| MoveTo (1, 1)
+      , Tile Stale (2, 2) 4 <| MoveTo (2, 1)
+      , Tile Stale (2, 4) 2 <| MoveTo (2, 2)
+      , Tile Stale (3, 2) 2 <| MoveTo (3, 1)
+      , Tile Stale (3, 4) 2 <| MoveTo (3, 1)
+      , Tile Stale (4, 1) 2 Stay
+      , Tile Composite (3, 1) 4 Stay
+      ]
+  }
+
+
+testCase3 : TestCase
+testCase3 =
+  { before =
+      [ Tile New (1, 1) 2 Stay
+      , Tile New (1, 2) 2 Stay
+      , Tile New (1, 3) 2 Stay
+      , Tile New (3, 2) 4 Stay
+      , Tile New (3, 3) 2 Stay
+      , Tile New (4, 4) 2 Stay
+      ]
+  , after =
+      [ Tile Stale (1, 1) 2 <| MoveTo (4, 1)
+      , Tile Stale (1, 2) 2 <| MoveTo (3, 2)
+      , Tile Stale (1, 3) 2 <| MoveTo (4, 3)
+      , Tile Stale (3, 2) 4 <| MoveTo (4, 2)
+      , Tile Stale (3, 3) 2 <| MoveTo (4, 3)
+      , Tile Stale (4, 4) 2 Stay
+      , Tile Composite (4, 3) 4 Stay
+      ]
+  }
+
+
+testCase4 : TestCase
+testCase4 =
+  { before =
+      [ Tile New (1, 4) 2 Stay
+      , Tile New (2, 2) 4 Stay
+      , Tile New (2, 3) 2 Stay
+      , Tile New (4, 1) 2 Stay
+      , Tile New (4, 2) 2 Stay
+      , Tile New (4, 3) 2 Stay
+      ]
+  , after =
+      [ Tile Stale (1, 4) 2 Stay
+      , Tile Stale (2, 2) 4 <| MoveTo (1, 2)
+      , Tile Stale (2, 3) 2 <| MoveTo (1, 3)
+      , Tile Stale (4, 1) 2 <| MoveTo (1, 1)
+      , Tile Stale (4, 2) 2 <| MoveTo (2, 2)
+      , Tile Stale (4, 3) 2 <| MoveTo (1, 3)
+      , Tile Composite (1, 3) 4 Stay
+      , Tile New (4, 1) 2 Stay
+      , Tile New (4, 4) 4 Stay
+      ]
+  }
+
+
+testCase5 : TestCase
+testCase5 =
+  { before =
+      [ Tile New (1, 1) 2 Stay
+      , Tile New (1, 2) 4 Stay
+      , Tile New (1, 3) 2 Stay
+      , Tile New (1, 4) 4 Stay
+      , Tile New (2, 1) 2 Stay
+      , Tile New (2, 2) 2 Stay
+      , Tile New (2, 3) 2 Stay
+      , Tile New (2, 4) 2 Stay
+      , Tile New (3, 1) 4 Stay
+      , Tile New (3, 2) 2 Stay
+      , Tile New (3, 3) 2 Stay
+      , Tile New (3, 4) 4 Stay
+      , Tile New (4, 1) 2 Stay
+      , Tile New (4, 2) 4 Stay
+      , Tile New (4, 3) 2 Stay
+      , Tile New (4, 4) 2 Stay
+      ]
+  , after =
+      [ Tile Stale (1, 1) 2 Stay
+      , Tile Stale (1, 2) 4 Stay
+      , Tile Stale (1, 3) 2 Stay
+      , Tile Stale (1, 4) 4 Stay
+      , Tile Stale (2, 1) 2 <| MoveTo (2, 3)
+      , Tile Stale (2, 2) 2 <| MoveTo (2, 3)
+      , Tile Stale (2, 3) 2 <| MoveTo (2, 4)
+      , Tile Stale (2, 4) 2 Stay
+      , Tile Stale (3, 1) 4 <| MoveTo (3, 2)
+      , Tile Stale (3, 2) 2 <| MoveTo (3, 3)
+      , Tile Stale (3, 3) 2 Stay
+      , Tile Stale (3, 4) 4 Stay
+      , Tile Stale (4, 1) 2 <| MoveTo (4, 2)
+      , Tile Stale (4, 2) 4 <| MoveTo (4, 3)
+      , Tile Stale (4, 3) 2 <| MoveTo (4, 4)
+      , Tile Stale (4, 4) 2 Stay
+      , Tile Composite (2, 3) 4 Stay
+      , Tile Composite (2, 4) 4 Stay
+      , Tile Composite (3, 3) 4 Stay
+      , Tile Composite (4, 4) 4 Stay
+      ]
+  }
 
 
 init : Model
 init =
-  Model
-    [ Tile (1, 1) 2 New
-    , Tile (1, 3) 2 New
-    ]
+  { testCase = testCase1
+  , grid = testCase1.before
+  }
 
 
 -- UPDATE
 
 
 type Msg
-  = ClickedMerge
+  = ClickedTest
+  | ClickedLoad1
+  | ClickedLoad2
+  | ClickedLoad3
+  | ClickedLoad4
+  | ClickedLoad5
 
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    ClickedMerge ->
-      { model
-      | tiles =
-          -- The order still matters.
-          [ Tile (1, 1) 2 <| MoveTo (1, 4)
-          , Tile (1, 3) 2 <| MoveTo (1, 4)
-          , Tile (1, 4) 4 Merged
-          ]
-          -- But, what's the order that works?
-      }
+    ClickedTest ->
+      { model | grid = model.testCase.after }
+
+    ClickedLoad1 ->
+      { model | testCase = testCase1, grid = testCase1.before }
+
+    ClickedLoad2 ->
+      { model | testCase = testCase2, grid = testCase2.before }
+
+    ClickedLoad3 ->
+      { model | testCase = testCase3, grid = testCase3.before }
+
+    ClickedLoad4 ->
+      { model | testCase = testCase4, grid = testCase4.before }
+
+    ClickedLoad5 ->
+      { model | testCase = testCase5, grid = testCase5.before }
 
 
 -- VIEW
 
 
 view : Model -> H.Html Msg
-view { tiles } =
+view { grid } =
   H.div []
-    [ H.div [ HA.class "row" ] <|
-        List.map viewTile tiles
-    , H.p [] [ H.button [ HE.onClick ClickedMerge ] [ H.text "Merge" ] ]
+    [ H.div [ HA.class "grid" ] <|
+        List.map viewTile grid
+    , H.p []
+        [ H.button [ HE.onClick ClickedLoad1 ] [ H.text "Load Test Case 1" ]
+        , H.button [ HE.onClick ClickedLoad2 ] [ H.text "Load Test Case 2" ]
+        , H.button [ HE.onClick ClickedLoad3 ] [ H.text "Load Test Case 3" ]
+        , H.button [ HE.onClick ClickedLoad4 ] [ H.text "Load Test Case 4" ]
+        , H.button [ HE.onClick ClickedLoad5 ] [ H.text "Load Test Case 5" ]
+        ]
+    , H.p [] [ H.button [ HE.onClick ClickedTest ] [ H.text "Test" ] ]
     ]
 
 
 viewTile : Tile -> H.Html msg
-viewTile { position, value, kind } =
+viewTile { kind, position, value, action } =
   let
     (r, c) =
       position
   in
   H.div
     [ HA.class "tile"
-    , HA.class <| "tile-" ++ String.fromInt r ++ "-" ++ String.fromInt c
     , HA.class <| "tile--" ++ String.fromInt value
     , HA.class <|
         case kind of
           New ->
             "tile--new"
 
-          MoveTo (mr, mc) ->
-            "tile-move-to-" ++ String.fromInt mr ++ "-" ++ String.fromInt mc
+          Stale ->
+            "tile--stale"
 
-          Merged ->
-            "tile--merged"
+          Composite ->
+            "tile--composite"
+    , HA.class <|
+        case action of
+          Stay ->
+            "tile-" ++ String.fromInt r ++ "-" ++ String.fromInt c
+
+          MoveTo (mr, mc) ->
+            "tile-" ++ String.fromInt mr ++ "-" ++ String.fromInt mc
     ]
     [ H.div [ HA.class "tile__inner" ] [] ]

a63624c7dadf1c3a001144dde2c15256fa1d09c3
Dwayne Crooks <me@dwaynecrooks.com>
2022-09-20 18:25:34 -0400

    Port the score to Elm

---
+++ src/App/Data/Points.elm
@@ -0,0 +1,40 @@
+module App.Data.Points exposing
+  ( Points
+  , zero, fromInt
+
+  , add, max
+
+  , toString
+  )
+
+
+type Points
+  = Points Int
+
+
+zero : Points
+zero =
+  Points 0
+
+
+fromInt : Int -> Points
+fromInt n =
+  if n > 0 then
+    Points n
+  else
+    Points 0
+
+
+add : Points -> Points -> Points
+add (Points a) (Points b) =
+  Points <| a + b
+
+
+max : Points -> Points -> Points
+max (Points a as p1) (Points b as p2) =
+  if a >= b then p1 else p2
+
+
+toString : Points -> String
+toString (Points n) =
+  String.fromInt n
---
+++ src/App/View/Score.elm
@@ -0,0 +1,101 @@
+module App.View.Score exposing
+  ( State
+  , init
+
+  , addDelta
+
+  , Msg
+  , update
+
+  , view, viewReadOnly
+  )
+
+
+import App.Data.Points as Points exposing (Points)
+import Html as H
+import Html.Attributes as HA
+import Html.Events as HE
+import Html.Keyed as HK
+import Json.Decode as JD
+
+
+type State
+  = State
+      { currentId : Int
+      , deltas : List (Int, Points)
+      }
+
+
+init : State
+init =
+  State
+    { currentId = 0
+    , deltas = []
+    }
+
+
+addDelta : Points -> State -> State
+addDelta points (State state) =
+  State
+    { state
+    | currentId = state.currentId + 1
+    -- NOTE: The following can be improved by using a queue.
+    , deltas = state.delta ++ [(state.currentId, points)]
+    }
+
+
+type Msg
+  = AnimationEnded
+
+
+update : Msg -> State -> State
+update msg (State state) =
+  case msg of
+    AnimationEnded ->
+      State { state | deltas = Maybe.withDefault [] <| List.tail deltas }
+
+
+view : String -> Points -> State -> H.Html Msg
+view title points (State { deltas }) =
+  H.div [ HA.class "score" ]
+    [ H.h2 [ HA.class "score__title" ] [ H.text title ]
+    , let
+        scoreValue =
+          ( "score__value"
+          , H.div [ HA.class "score__value" ]
+              [ H.text <| Points.toString points ]
+          )
+
+        scoreDeltas =
+          List.map viewScoreDelta deltas
+      in
+      HK.node "div" [ HA.class "score__total" ] <|
+        scoreValue :: scoreDeltas
+    ]
+
+
+viewScoreDelta : (Int, Points) -> (String, H.Html Msg)
+viewScoreDelta (id, points) =
+  ( String.fromInt id
+  , H.div
+      [ HA.class "score__delta"
+      , onAnimationEnd AnimationEnded
+      ]
+      [ H.text <| "+" ++ Points.toString points ]
+  )
+
+
+onAnimationEnd : msg -> H.Attribute msg
+onAnimationEnd msg =
+  HE.on "animationend" <| JD.succeed msg
+
+
+viewReadOnly : String -> Points -> H.Html msg
+viewReadOnly title points =
+  H.div [ HA.class "score" ]
+    [ H.h2 [ HA.class "score__title" ] [ H.text title ]
+    , H.div [ HA.class "score__total" ]
+        [ H.div [ HA.class "score__value" ]
+            [ H.text <| Points.toString points ]
+        ]
+    ]

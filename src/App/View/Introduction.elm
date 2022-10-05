f0f91de37170477ab74d117bc1a5d9758f2e8556
Dwayne Crooks <me@dwaynecrooks.com>
2022-10-05 06:23:18 -0400

    Review and refactor the introduction block

--- src/App/View/Introduction.elm
+++ src/App/View/Introduction.elm
@@ -9,13 +9,13 @@ import Html.Attributes as HA
 view : msg -> H.Html msg
 view onNewGame =
   H.div [ HA.class "introduction" ]
-    [ H.div [ HA.class "introduction__item" ]
+    [ H.div [ HA.class "introduction__paragraph" ]
         [ H.p [ HA.class "m0" ]
             [ H.text "Join the numbers and get to the "
             , H.strong [ HA.class "nowrap" ] [ H.text "2048 tile!" ]
             ]
         ]
-    , H.div [ HA.class "introduction__item" ]
+    , H.div [ HA.class "introduction__button" ]
         [ Button.view
             { text = "New Game"
             , onClick = onNewGame

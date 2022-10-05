aa359e80012fab35d4cbd0ea5d8f3b179ccd5124
Dwayne Crooks <me@dwaynecrooks.com>
2022-10-05 06:56:22 -0400

    Review and refactor the message block

--- src/App/View/Message.elm
+++ src/App/View/Message.elm
@@ -16,41 +16,60 @@ type alias GameOverOptions msg =
 
 viewGameOver : GameOverOptions msg -> H.Html msg
 viewGameOver { onTryAgain } =
-  H.div [ HA.class "message" ]
-    [ H.h2 [ HA.class "message__title" ] [ H.text "Game over!" ]
-    , H.div [ HA.class "message__controls" ]
-        [ H.div [ HA.class "message__button" ]
-            [ Button.view
-                { text = "Try again"
-                , onClick = onTryAgain
-                }
-            ]
+  view
+    { isSuccess = False
+    , title = "Game over!"
+    , buttonOptions =
+        [ { text = "Try again"
+          , onClick = onTryAgain
+          }
         ]
-    ]
+    }
 
 
 type alias WinOptions msg =
-  { onTryAgain : msg
-  , onKeepPlaying : msg
+  { onKeepPlaying : msg
+  , onTryAgain : msg
   }
 
 
 viewWin : WinOptions msg -> H.Html msg
-viewWin { onTryAgain, onKeepPlaying } =
-  H.div [ HA.class "message message--success" ]
-    [ H.h2 [ HA.class "message__title" ] [ H.text "You win!" ]
-    , H.div [ HA.class "message__controls" ]
-        [ H.div [ HA.class "message__button" ]
-            [ Button.view
-                { text = "Keep going"
-                , onClick = onKeepPlaying
-                }
-            ]
-        , H.div [ HA.class "message__button" ]
-            [ Button.view
-                { text = "Try again"
-                , onClick = onTryAgain
-                }
-            ]
+viewWin { onKeepPlaying, onTryAgain } =
+  view
+    { isSuccess = True
+    , title = "You win!"
+    , buttonOptions =
+        [ { text = "Keep going"
+          , onClick = onKeepPlaying
+          }
+        , { text = "Try again"
+          , onClick = onTryAgain
+          }
+        ]
+    }
+
+
+type alias Options msg =
+  { isSuccess : Bool
+  , title : String
+  , buttonOptions : List (Button.Options msg)
+  }
+
+
+view : Options msg -> H.Html msg
+view { isSuccess, title, buttonOptions } =
+  H.div
+    [ HA.class "message"
+    , HA.classList
+        [ ("message--success", isSuccess)
         ]
     ]
+    [ H.h2 [ HA.class "message__title" ] [ H.text title ]
+    , H.div [ HA.class "message__controls" ] <|
+        List.map viewMessageButton buttonOptions
+    ]
+
+
+viewMessageButton : Button.Options msg -> H.Html msg
+viewMessageButton options =
+  H.div [ HA.class "message__button" ] [ Button.view options ]
